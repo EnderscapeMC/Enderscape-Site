76f83ac27ec50f051bcffe374365661134bfd8ab
Lydia <lydiakristinee@outlook.com>
2023-06-29 16:16:45 +0930

    Styling

--- main.php
+++ main.php
@@ -4,6 +4,7 @@
         <link rel="stylesheet" type="text/css" href="css/index.css">
         <link rel="icon" type="image/x-icon" href="images/enderscape-logo.gif">
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home</title>
         <script src="js/index.js"></script> 
     </head>
