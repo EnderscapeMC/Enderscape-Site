46fc0fd666def5e677e655b932e6f8486baf8942
Lydia <lydiakristinee@outlook.com>
2023-06-30 19:59:23 +0930

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var header = document.getElementById('header');
+var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+header.style.height = windowHeight + 'px';
+
 function responsiveNav() {
   var x = document.getElementById("topnav");
   if (x.className === "topnav") {
