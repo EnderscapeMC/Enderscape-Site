2abea8d325f8418c8d784f9fc48b0305361d0eb3
Lydia <lydiakristinee@outlook.com>
2023-07-11 18:05:45 +0930

    Update main.php

--- main.php
+++ main.php
@@ -105,12 +105,12 @@
                 <section class="col-3">
                     <h1>Store</h1>
                 </section>
-                <section class="col-5">
+                <section class="col-4">
                     <p>Here you can buy a variety of in-game products for real-world currency.</p>
                     <p>By purchasing from our store, you are supporting the server, ensuring it stays online for years to come.</p>
                     <button type="button" onclick="window.open('https://store.enderscape.net/', '_blank')">Visit Store</button>
                 </section>
-                <div class="col-2"></div>
+                <div class="col-3"></div>
             </div>
         </div>
         <div id="leaderboards">
@@ -161,12 +161,12 @@
                 <section class="col-3">
                     <h1>Wiki</h1>
                 </section>
-                <section class="col-5">
+                <section class="col-4">
                     <p>Ever wondered how the Lands menu works or how to make more money on the server?</p>
                     <p>The Enderscape Wiki is currently being built and will contain everything you need to know about the server and it's features.</p>
                     <button type="button" onclick="window.open('https://docs.enderscape.net', '_blank')">Visit Wiki</button>
                 </section>
-                <div class="col-2"></div>
+                <div class="col-3"></div>
             </div>
         </div>
         <hr>
@@ -176,12 +176,12 @@
                 <section class="col-3">
                     <h1>Vote</h1>
                 </section>
-                <section class="col-5">
+                <section class="col-4">
                     <p>Vote for Enderscape and get rewarded with in-game perks.</p>
                     <p>By voting you can earn exclusive vote tags to showcase your dedication and Cubelet keys to unlock randomised prizes.</p>
                     <button type="button" onclick="window.open('../html/vote.php', '_blank')">Vote Now</button>
                 </section>
-                <div class="col-2"></div>
+                <div class="col-3"></div>
             </div>
         </div>
         <hr>
@@ -191,12 +191,12 @@
                 <section class="col-3">
                     <h1>Discord</h1>
                 </section>
-                <section class="col-5">
+                <section class="col-4">
                     <p>Join the server's Discord to connect with other players, have your say on the server and stay up to date with changes and announcements.</p>
                     <p>You can link your Minecraft account with our Discord server by typing /link in game.</p>
                     <button type="button" onclick="window.open('https://discord.gg/nHmYSDd8ms', '_blank')">Join the Discord</button>
                 </section>
-                <div class="col-2"></div>
+                <div class="col-3"></div>
             </div>
         </div>
         <div id="quizzes">
@@ -210,7 +210,7 @@
                     <p>Test your Enderscape knowledge with our collection of quizzes.</p>
                     <button type="button" onclick="window.open('#', '_blank')">Suggest a Quiz</button>
                 </section>
-                <section class="col-5">
+                <section class="col-4">
                     <ul>
                         <a href="../html/lore-quiz.html">
                             <li>
@@ -241,7 +241,7 @@
                         </a>
                     </ul>
                 </section>
-                <div class="col-2"></div>
+                <div class="col-3"></div>
             </div>
             <svg viewBox="0 0 1200 120" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" class="top">
                 <path d="M0 88L25 93.2C50 98.3 100 108.7 150 110C200 111.3 250 103.7 300 96.7C350 89.7 400 83.3 450 85.3C500 87.3 550 97.7 600 104.2C650 110.7 700 113.3 750 113.8C800 114.3 850 112.7 900 107.8C950 103 1000 95 1050 94.2C1100 93.3 1150 99.7 1175 102.8L1200 106L1200 121L1175 121C1150 121 1100 121 1050 121C1000 121 950 121 900 121C850 121 800 121 750 121C700 121 650 121 600 121C550 121 500 121 450 121C400 121 350 121 300 121C250 121 200 121 150 121C100 121 50 121 25 121L0 121Z" fill="#191919" stroke-linecap="round" stroke-linejoin="miter"></path>
